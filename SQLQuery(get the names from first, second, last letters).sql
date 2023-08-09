7c7a387e26d4c585e1ff0c61be211d444647bdd1
h7m99 <138456646+h7m99@users.noreply.github.com>
2023-08-09 11:53:16 +0400

    Add files via upload

---
+++ SQLQuery(get the names from first, second, last letters).sql
@@ -0,0 +1,9 @@
+select * from Customer where firstname  like 'a_%'; --first letter from the first name--
+
+select * from Customer where firstname  like '_a%'; --second letter from the first name--
+
+select * from Customer where firstname  like '_%a'; --last letter from the first name--
+
+
+
+
